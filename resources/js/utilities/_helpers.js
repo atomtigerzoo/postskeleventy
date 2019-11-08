048a0465e94224173c3147d76dcea4f5d0990d4d
Henning Stein <stein@atomtigerzoo.com>
2019-11-08 15:05:58 +0100

    some last linting on js files

--- resources/js/utilities/_helpers.js
+++ resources/js/utilities/_helpers.js
@@ -1,20 +1,15 @@
-
 // Helpers
-const
 
-    /**
-	 * @description Test if the body id is something
-	 * @param  		{string}
-	 * @return 		{bool}
-	 *
-	 */
+/**
+ * @description Test if the body id is something
+ * @param  		{string}
+ * @return 		{bool}
+ *
+ */
+const page = function page(name) {
+  if (!name) {
+    return $body.getAttribute('id');
+  }
 
-    page = function page(name) {
-
-        if (!name) {
-            return $body.getAttribute('id')
-        }
-
-        return ($body.getAttribute('id') == name);
-
-    };
+  return ($body.getAttribute('id') == name);
+};
